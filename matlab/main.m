322e676260f5c7738c80170951da16df39ef29bd
BaptCav <Baptiste.cavarec@orange.fr>
2015-12-12 13:13:27 +0100

    corection de ma faute de cotorep

--- matlab/main.m
+++ matlab/main.m
@@ -1,7 +1,8 @@
 clear all;clc;
 filename='thank.wav';
+filename='orinoccio.wav';
 [y,Fs,nbits]=wavread(filename);
-%soundsc(y,Fs);
+soundsc(y,Fs);
 
 [y0,y1] = H1andH0(y);
 y1d=downsample(y1,2);
@@ -16,18 +17,20 @@ y01u=upsample(y01,2);
 
 y0r = G0andG1(y00u,y01u);
 e0=mean((y0d-y0r)'*((y0d-y0r)))/length(y0);
-%y1d=[y1d(4:length(y1d));y1d(length(y1d));y1d(length(y1d));y1d(length(y1d))]; %%% Correspond a leur histoire de delay
+y1d=[y1d(4:length(y1d));y1d(length(y1d));y1d(length(y1d));y1d(length(y1d))]; %%% Correspond a leur histoire de delay
 y0ru=upsample(y0r,2);
 y1du=upsample(y1d,2);
-yr = G0andG1(y1du,y0ru);
+yr = G0andG1(y0ru,y1du);
 
 yr=yr(7:length(yr));
-e=mean((y(7:length(y))-yr)'*((y(7:length(y))-yr)))/length(y);
-soundsc(yr,Fs);
+e=mean((y(1:length(y)-6)-yr)'*((y(1:length(y)-6)-yr)))/length(y);
+
+
+%soundsc(yr,Fs);
 
 figure
 subplot(211)
-plotspectrum(y);
+plotspectrum(y(1:length(y)-6));
 hold on
 subplot(212);
 plotspectrum(yr)
